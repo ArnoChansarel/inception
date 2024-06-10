804b5b9ba80b0c1f4440031509c9f9bf4c9f264d
Arno Chansarel <chansarelarno@gmail.com>
2024-06-10 10:23:55 -0400

    clean version

--- delete_dock.sh
+++ delete_dock.sh
@@ -2,6 +2,6 @@ docker stop $(docker ps -q)
 docker rm $(docker ps -a -q)
 docker rmi -f $(docker images -a -q)
 docker volume rm $(docker volume ls -q)
-docker network rm $(docker network ls -q)
 docker system prune -a --volumes
 docker system prune -a --force
+docker network rm $(docker network ls -a -q)
